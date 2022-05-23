f1875ace9062fe421e2ea07cd5d51fffe57b730a
ilmari-h <52321471+ilmari-h@users.noreply.github.com>
2022-05-23 19:05:38 +0300

    Add font link to document head section.

--- src/renderPage.js
+++ src/renderPage.js
@@ -23,6 +23,12 @@ export default function Page({
           <title>Ilmari.dev</title>
           <meta name="description" content="Developer site" />
           <link rel="icon" href="/code-solid.svg" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         {children}
       </div>
