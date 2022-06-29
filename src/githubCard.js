a382de68a5d792c07765569b9d66fbc56e215c5f
ilmari-h <52321471+ilmari-h@users.noreply.github.com>
2022-06-29 22:00:52 +0300

    Update github card ui.

--- src/githubCard.js
+++ src/githubCard.js
@@ -5,6 +5,7 @@ import {
   BsShieldShaded,
   BsFillStarFill,
 } from "react-icons/bs";
+import { IoGitBranch } from "react-icons/io5";
 
 export default function GithubCard({ repoInfo }) {
   return (
@@ -14,6 +15,18 @@ export default function GithubCard({ repoInfo }) {
       </div>
       <p>{repoInfo.description}</p>
       <div className={styles.bottomRow}>
+        {repoInfo.stargazers_count > 0 && (
+          <div className={styles.ghStat}>
+            <BsFillStarFill /> <span>{repoInfo.stargazers_count}</span>
+          </div>
+        )}
+
+        {repoInfo.forks_count > 0 && (
+          <div className={styles.ghStat}>
+            <IoGitBranch /> <span>{repoInfo.forks_count}</span>
+          </div>
+        )}
+
         <div className={styles.ghStat}>
           <BsCodeSlash /> <span>{repoInfo.language}</span>
         </div>
@@ -24,18 +37,11 @@ export default function GithubCard({ repoInfo }) {
           </div>
         )}
 
-        {repoInfo.stargazers_count > 0 && (
-          <div className={styles.ghStat}>
-            <BsFillStarFill />{" "}
-            <span style={{ marginLeft: 4 }}>{repoInfo.stargazers_count}</span>
-          </div>
-        )}
-
         <a
           className={styles.ghStat}
           href={`https://github.com/ilmari-h/${repoInfo.name}`}
         >
-          <BsGithub /> <span>Source</span>
+          <BsGithub /> <span>Repo</span>
         </a>
       </div>
     </div>
